078f1db15ff2b49ee3bb4a8c4305e079f59746bd
Hammad <hammad@pukat.net>
2024-05-27 15:36:47 +0500

    fix mobile validation

--- utils/validations.js
+++ utils/validations.js
@@ -7,7 +7,7 @@ const registerSchema = Joi.object({
   firstName: Joi.string().required(),
   lastName: Joi.string().required(),
   email: Joi.string().email().regex(emailRegex).required(),
-  mobile: Joi.number().required().max(12).min(11), // You might want to add validation specific to mobile numbers
+  mobile: Joi.number().required().min(11), // You might want to add validation specific to mobile numbers
   password: Joi.string().required().min(6).max(12), // You might want to add more password constraints
 });
 
